c85dec27c9c0f50f18522b25b174eaaf51e52bdb
vincenzo <vnzstc@protonmail.com>
2026-09-03 16:32:05 +0000

    profiling script completed

--- profile.sh
+++ profile.sh
@@ -14,11 +14,20 @@ fi
 args=("$@")
 cmd="${args[@]:1:3}"
 
-if [[ "${args[1]}" == "xz" ]]; then
-    cmd="${cmd} > ${args[4]}"
+#if [[ "${args[1]}" == "xz" || "${args[1]}" == "gzip" ]]; then
+#    cmd="${cmd} > ${args[4]}output.tar"
+#else
+#    cmd="${cmd} -C ${args[4]}"
+#fi
+#
+
+if [[ "${args[1]}" == "tar" ]] then
+	cmd="${cmd} -C ${args[4]}"
 else
-    cmd="${cmd} -C ${args[4]}"
+	cmd="${cmd} > ${args[4]}output.tar"
 fi
+
+echo ${cmd}
 
 ## perf stat -a -e power/energy-pkg/,power/energy-ram/,cycles,instructions,cache-references,cache-misses,page-faults,context-switches -o $parent/stat.txt -- $*
 perf stat -e power/energy-pkg/,power/energy-ram/ -o "${args[0]}/stat.txt" -- bash -c "$cmd"
